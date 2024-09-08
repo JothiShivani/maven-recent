529aff0c175a41f3b89634c56e0fa1a4de6a2f50
JothiShivani <jothishivani123@gmail.com>
2024-09-09 00:02:48 +0530

    first commit

--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
+  region     = "us-east-1"  # Set your AWS region here
+  access_key = "AKIAYZZGTFDYMZ2VQEWI"  # Replace with your AWS access key
+  secret_key = "QsSBgjbTzzGgc70u3kgCF8qX9l6FlUCUJgVlva2j"  # Replace with your AWS secret key
 }
