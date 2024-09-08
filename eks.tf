455e0a79a2884eb1068a77d07df9a8f9cd2eeffb
JothiShivani <jothishivani123@gmail.com>
2024-09-08 23:21:50 +0530

    first commit

--- eks.tf
+++ eks.tf
@@ -1,3 +1,37 @@
+# IAM Role Policy Attachment for EKS Cluster
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  role       = "clusterrole_one"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  role       = "clusterrole_one"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+# IAM Role Policy Attachment for Node Group
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  role       = "new_noderule"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  role       = "new_noderule"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  role       = "new_noderule"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEBSCSIDriverPolicy" {
+  role       = "new_noderule"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
+
 resource "aws_eks_cluster" "example" {
   name     = "example"
   role_arn = "arn:aws:iam::605134465264:role/clusterrole_one"
@@ -8,11 +42,11 @@ resource "aws_eks_cluster" "example" {
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  # depends_on = [
+  #   aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+  #   aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
     
-  ]
+  # ]
 }
 
 # output "endpoint" {
@@ -41,10 +75,11 @@ resource "aws_eks_node_group" "new-nodegroup" {
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.example-AmazonEBSCSIDriverPolicy,
-  ]
+  # depends_on = [
+  #   aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+  #   aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+  #   aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  #   aws_iam_role_policy_attachment.example-AmazonEBSCSIDriverPolicy,
+  # ]
+
 }
